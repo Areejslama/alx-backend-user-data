e12f3b200fa2522b6ba277397cd3b2626f37eb5d
Areejslama <areegahmed449@gmail.com>
2024-08-30 03:55:20 +0300

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-"""this script define function"""
+"""
+This script defines a function to obfuscate sensitive data"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """define function"""
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscates sensitive fields"""
     for field in fields:
         pattern = rf'{field}=[^{separator}]*'
         message = re.sub(pattern, f'{field}={redaction}', message)

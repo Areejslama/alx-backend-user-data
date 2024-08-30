307bd17d31c16a48a241f09b2a08b6679b1b2d94
Areejslama <areegahmed449@gmail.com>
2024-08-30 03:42:28 +0300

    0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""this script define function"""
+import logging
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """define function"""
+    for field in fields:
+        pattern = rf'{field}=[^{separator}]*'
+        message = re.sub(pattern, f'{field}={redaction}', message)
+
+    return message

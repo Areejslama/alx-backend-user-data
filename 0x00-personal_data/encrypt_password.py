1215d3eda3b599edd2a0a0e308f9e6f1c80ea4ba
Areejslama <areegahmed449@gmail.com>
2024-09-01 03:38:57 +0300

    pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ def hash_password(password: str) -> bytes:
     password_bytes = password.encode('utf-8')
     hashed = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """define function"""
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)
+

2d544c648b2cdefa0ddf98e6ec885ff855f15e5d
Areejslama <areegahmed449@gmail.com>
2024-09-03 00:31:13 +0300

    4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,10 +15,12 @@ class Auth:
         if excluded_paths is None or not excluded_paths:
             return True
 
+        if not path.endswith('/'):
+            path += '/'
+
         for excluded_path in excluded_paths:
             if fnmatch.fnmatch(path, excluded_path):
                 return False
-
         return True
 
     def authorization_header(self, request=None) -> str:

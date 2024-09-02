6c35dbb02362ec1a2c70216a59f6aa2d2e4402af
Areejslama <areegahmed449@gmail.com>
2024-09-02 22:49:31 +0300

    auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""define auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """define class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """return require auth"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """define header"""
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """define user"""
+        return None

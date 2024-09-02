1a674ab0e25bbff458da4a749a1a79e83b89df20
Areejslama <areegahmed449@gmail.com>
2024-09-02 22:51:03 +0300

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,11 +9,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """return require auth"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """define header"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """define user"""
         return None

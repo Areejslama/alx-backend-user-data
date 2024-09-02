94d3faeaa1abf534a6cec7f5eae972c6971f1798
Areejslama <areegahmed449@gmail.com>
2024-09-02 21:52:19 +0300

    auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
-def authorized() -> None:
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
     """define method"""
     abort(401)

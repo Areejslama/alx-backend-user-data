2a740eaabca9798039ea98ffb0f73403ddb3640c
Areejslama <areegahmed449@gmail.com>
2024-09-03 22:12:10 +0300

    10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """this script to define class"""
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -44,3 +45,21 @@ class BasicAuth(Auth):
             return None, None
         user, password = decoded_base64_authorization_header.split(':', 1)
         return user, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """define method"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        if users and users[0].is_valid_password(user_pwd):
+            return users[0]
+
+        return None

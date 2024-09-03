253f056fed8b80b3c7250892eae5d5b65e5f259c
Areejslama <areegahmed449@gmail.com>
2024-09-03 17:05:06 +0300

    8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """this script to define class"""
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """define class"""
@@ -15,3 +15,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """define method"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+
+        except Exception:
+            return None

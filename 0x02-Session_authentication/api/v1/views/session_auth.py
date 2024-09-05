756e9430f741cca15e2d71b55db748175560d773
Areejslama <areegahmed449@gmail.com>
2024-09-05 03:05:45 +0300

    7

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""this script to Create a new Flask view"""
+from flask import request, jsonify
+from api.v1.app import auth
+from api.v1.views import app_views
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def get_user() -> str:
+    """Handle user login."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        session_id = auth.create_session(user.id)
+        response = jsonify(user.to_json())
+        session_name = getenv('SESSION_NAME')
+
+        response.set_cookie(session_name, session_id)
+
+    return response

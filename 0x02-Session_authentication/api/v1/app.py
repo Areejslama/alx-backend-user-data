30da0ca8d7a47fe85e90234d726ddc1fa6313d08
Areejslama <areegahmed449@gmail.com>
2024-09-06 14:43:16 +0300

    me

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
+auth_type = getenv('AUTH_TYPE')
 if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':

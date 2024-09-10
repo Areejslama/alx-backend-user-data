53b38fa14664b8bb108a118ba62533219c75c5be
Areejslama <areegahmed449@gmail.com>
2024-09-10 23:29:55 +0300

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 import bcrypt
 from db import DB
 from user import Base, User
+from db import find_user_by
+from user import User
+
 
 def _hash_password(password: str) -> bytes:
     """define function"""
@@ -20,4 +23,10 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """register the added user"""
-        
+        new_user = self._db.find_user_by(email)
+        if new_user:
+            raise ValueError("User <user's email> already exists")
+        else:
+            new_password = self._hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+        return user

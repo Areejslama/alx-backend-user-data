f43f31e621667cbdde63d793b442c55e3d283374
Areejslama <areegahmed449@gmail.com>
2024-09-11 17:36:05 +0300

    11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,23 +29,16 @@ def users():
         abort(400)
 
 
-@app.route("/sessions", methods=['POST'])
-def login():
-    """Handle user login."""
-    if request.method == 'POST':
-        email = request.form.get("email")
-        password = request.form.get("password")
-        if not email or not password:
-            abort(400)
-            user = AUTH.valid_login("email", "password")
-            if not user:
-                abort(401)
-            session_id = AUTH.create_session(email)
-            response = make_response(jsonify({"email": email,
-                                              "message": "logged in"}))
-            response.set_cookie("session_id", session_id)
-
-        return response
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """define method"""
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":

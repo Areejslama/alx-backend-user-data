4bf5855822809e811e70d877fe2887524a951c43
Areejslama <areegahmed449@gmail.com>
2024-09-12 03:39:08 +0300

    19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,14 +83,14 @@ def get_reset_password_token():
 @app.route('/reset_password', methods=['PUT'])
 def update_password():
     """define method"""
-    try:
         email = request.form.get("email")
         reset_token = request.form.get("reset_token")
-        new_password =  request.form.get("new_password")
-        AUTH.update_password(reset_token, new_password)
-    except Exception:
-        abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
+        new_password = request.form.get("new_password")
+        try:
+            AUTH.update_password(reset_token, new_password)
+        except Exception:
+            abort(403)
+        return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":

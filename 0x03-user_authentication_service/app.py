661d02af5cdd99e409ab4740cb764eec691db61f
Areejslama <areegahmed449@gmail.com>
2024-09-14 04:18:39 +0300

    19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -104,9 +104,9 @@ def update_password() -> str:
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
-    except ValueError:
+    except Exception:
         abort(403)
+        return jsonify({"email": user_email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
